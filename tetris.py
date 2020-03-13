43c4c09689ea0160dcff74089582a8f5894c8242
Jan Lewandowski <j.lewandowski.5@stud.elka.pw.edu.pl>
2020-03-13 15:48:02 +0100

    created classes block and shape

---
+++ tetris.py
@@ -0,0 +1,24 @@
+# single block class in tetris game
+class Block(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+
+# single shape class in tetris game
+class Shape(object):
+    blocks = [4]
+
+
